0bd00904627e94d0e27fcc196b043872ab16464f
Miguel G. Oliveira <37053606+migueljgoliveira@users.noreply.github.com>
2021-02-25 17:49:50 +0000

    implemented option to terminate analysis if rupture limit is reached

--- source/ummdp_rupture.f
+++ source/ummdp_rupture.f
@@ -24,43 +24,57 @@ c-----------------------------------------------------------------------
 c
       dimension UVAR1(NUVARM),JMAC(*),JMATYP(*)
       dimension sdv(nsdv),uvar2(nuvarm),prrup(ndrup)
+			real*8 lim,wlimnorm
 c-----------------------------------------------------------------------
 c
-      ntrup = prrup(1)
+c      prrup(1) : criteria id
+c      prrup(2) : flag to terminate analysis if limit is reached
+c      prrup(3) : rupture limit
 c
+c 																		       ---- rupture criteria limit
+			lim = prrup(3)
+c                                           ---- select rupture criteria
+			ntrup = nint(prrup(1))
       select case ( ntrup )
+c
       case ( 0 )                                  ! No Rupture Criterion
         return
 c
       case ( 1 )                             ! Equivalent Plastic Strain
         call jancae_rup_eqstrain ( sdv,nsdv,uvar2,uvar1,nuvarm,nt,
-     &                             ndrup,prrup )
+     &                             lim,wlimnorm )
 c
       case ( 2 )                                   ! Cockroft and Latham
         call jancae_rup_cockroft ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                             jmac,jmatyp,matlayo,laccfla,nt,
-     &                             ndrup,prrup )
+     &                             lim,wlimnorm )
 c
       case ( 3 )                                       ! Rice and Tracey
         call jancae_rup_rice ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                         jmac,jmatyp,matlayo,laccfla,nt,
-     &                         ndrup,prrup )
+     &                         lim,wlimnorm )
 c
       case ( 4 )                                                 ! Ayada
         call jancae_rup_ayada ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                          jmac,jmatyp,matlayo,laccfla,nt,
-     &                          ndrup,prrup )
+     &                          lim,wlimnorm )
 c
       case ( 5 )                                                ! Brozzo
         call jancae_rup_brozzo ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                           jmac,jmatyp,matlayo,laccfla,nt,
-     &                           ndrup,prrup )
+     &                           lim,wlimnorm )
 c
       case default
         write (6,*) 'error in jancae_rupture'
         write (6,*) 'ntrup error :',ntrup
-        call jancae_exit (9000)
+        call jancae_exit ( 9000 )
       end select
+c
+c                    ---- terminate analysis if rupture limit is reached
+			end = nint(prrup(2))
+			if ( end .eq. 1 ) then
+				if ( wlimnorm .ge. 1.0d0 ) call jancae_exit( 10000 )
+			end if
 c
       return
       end
@@ -89,22 +103,27 @@ c
       case ( 1 ) 														 ! Equivalent Plastic Strain
         write (6,*) 'Equivalent Plastic Strain'
         write (6,*) 'W=int[dp]'
+				write (6,*) 'Wl=',prrup(3)
 c
       case ( 2 )  																 ! Cockroft and Latham
         write (6,*) 'Cockroft and Latham'
         write (6,*) 'W=int[(sp1/se)*dp]'
+				write (6,*) 'Wl=',prrup(3)
 c
       case ( 3 ) 																	     ! Rice and Tracey
         write (6,*) 'Rice and Tracey'
         write (6,*) 'W=int[exp(1.5*sh/se)*dp]'
+				write (6,*) 'Wl=',prrup(3)
 c
       case ( 4 ) 																						     ! Ayada
         write (6,*) 'Ayada'
         write (6,*) 'W=int[(sh/se)*dp]'
-c
+				write (6,*) 'Wl=',prrup(3)
+
       case ( 5 ) 																							  ! Brozzo
         write (6,*) 'Brozzo'
         write (6,*) 'W=int[(2/3)*(sp1/(sp1-se))*dp]'
+				write (6,*) 'Wl=',prrup(3)
       end select
 c
       return
@@ -116,12 +135,12 @@ c-----------------------------------------------------------------------
 c     Equivalent Plastic Strain
 c
       subroutine jancae_rup_eqstrain ( sdv,nsdv,uvar2,uvar1,nuvarm,nt,
-     &                                 ndrup,prrup)
+     &                                 lim,wlimnorm )
 c
 c-----------------------------------------------------------------------
       implicit real*8 (a-h,o-z)
 c
-      dimension sdv(nsdv),uvar2(nuvarm),uvar1(nuvarm),prrup(ndrup)
+      dimension sdv(nsdv),uvar2(nuvarm),uvar1(nuvarm)
       real*8 lim,peeq
 c-----------------------------------------------------------------------
 c
@@ -130,11 +149,9 @@ c
 c     uvar(2+nt+1) : equivalent plastic strain
 c     uvar(2+nt+2) : rupture criterion normalised
 c
-c     prrup(1) : 1
-c     prrup(2) : rupture criterion limit
+c                                            ---- get uvar before update
+      wlimnorm  = uvar1(2+nt+2)
 c
-c                                           ---- rupture criterion limit
-      lim = prrup(2)
 c                                              ---- get sdv after update
       peeq = sdv(1)
 c
@@ -142,50 +159,45 @@ c                                                       ---- update uvar
       uvar2(2+nt+1) = peeq
       uvar2(2+nt+2) = peeq / lim
 c
-c      											 ---- terminate analisys if limit is reached
-			if ( uvar1(2+nt+2) .ge. 1.0d0 ) call jancae_exit( 9000 )
-c
       return
       end
 c
 c
 c
 c-----------------------------------------------------------------------
-c     Cockroft and Latham (CL)
+c     Cockroft and Latham
 c
       subroutine jancae_rup_cockroft ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                                 jmac,jmatyp,matlayo,laccfla,nt,
-     &                                 ndrup,prrup )
+     &                                 lim,wlimnorm )
 c
 c-----------------------------------------------------------------------
       implicit real*8 (a-h,o-z)
 c
       dimension UVAR1(NUVARM),JMAC(*),JMATYP(*)
-      dimension ARRAY(15),JARRAY(15),prrup(ndrup)
+      dimension ARRAY(15),JARRAY(15)
       dimension sdv(nsdv),uvar2(nuvarm)
       character*3 FLGRAY(15)
-      real*8 lim
+      real*8 lim,wlimnorm
       real*8 se1,peeq1,maxsp1,maxsp1se1,wlim1
       real*8 se2,peeq2,maxsp2,maxsp2se2,wlim2
 c-----------------------------------------------------------------------
 c
 c     nuvarm : 4
 c
+c     uvar(1)      : equivalent stress
 c     uvar(2+nt+1) : equivalent plastic strain
 c     uvar(2+nt+2) : maximum principal stress
 c     uvar(2+nt+3) : rupture criterion
 c     uvar(2+nt+4) : rupture criterion normalised
 c
-c     prrup(1) : 2
-c     prrup(2) : rupture criterion limit
-c
-c                                           ---- rupture criterion limit
-      lim = prrup(2)
 c                                            ---- get uvar before update
       se1    = uvar1(1)
       peeq1  = uvar1(2+nt+1)
       maxsp1 = uvar1(2+nt+2)
       wlim1  = uvar1(2+nt+3)
+c
+			wlimnorm  = uvar1(2+nt+4)
 c
 c                                     ---- get sdv and uvar after update
       se2 = uvar2(1)
@@ -208,14 +220,12 @@ c                                                 ---- rupture criterion
       if ( se2 .gt. 0.0d0 ) maxsp2se2 = maxsp2 / se2
 c
       wlim2 = wlim1 + (maxsp2se2+maxsp1se1)*(peeq2-peeq1)/2.0d0
+c
 c                                                       ---- update uvar
       uvar2(2+nt+1) = peeq2
       uvar2(2+nt+2) = maxsp2
       uvar2(2+nt+3) = wlim2
       uvar2(2+nt+4) = wlim2/lim
-c
-c      											 ---- terminate analisys if limit is reached
-			if ( uvar1(2+nt+2) .ge. 1.0d0 ) call jancae_exit( 9000 )
 c
       return
       end
@@ -223,41 +233,39 @@ c
 c
 c
 c-----------------------------------------------------------------------
-c     Rice and Tracey (RT)
+c     Rice and Tracey
 c
       subroutine jancae_rup_rice ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                             jmac,jmatyp,matlayo,laccfla,nt,
-     &                             ndrup,prrup )
+     &                             lim,wlimnorm )
 c
 c-----------------------------------------------------------------------
       implicit real*8 (a-h,o-z)
 c
       dimension UVAR1(NUVARM),JMAC(*),JMATYP(*)
-      dimension ARRAY(15),JARRAY(15),prrup(ndrup)
+      dimension ARRAY(15),JARRAY(15)
       dimension sdv(nsdv),uvar2(nuvarm)
       character*3 FLGRAY(15)
-      real*8 lim
+      real*8 lim,wlimnorm
       real*8 se1,peeq1,shyd1,shyd1se1,wlim1
       real*8 se2,peeq2,shyd2,shyd2se2,wlim2
 c-----------------------------------------------------------------------
 c
 c     nuvarm : 4
 c
-c     uvar(2+nt+1) : eq. plastic strain
+c     uvar(1)      : equivalent stress
+c     uvar(2+nt+1) : equivalent plastic strain
 c     uvar(2+nt+2) : hydrostatic stress
 c     uvar(2+nt+3) : rupture criterion
 c     uvar(2+nt+4) : rupture criterion normalised
 c
-c     prrup(1) : 3
-c     prrup(2) : rupture criterion limit
-c
-c                                           ---- rupture criterion limit
-      lim = prrup(2)
 c                                            ---- get uvar before update
       se1   = uvar1(1)
       peeq1 = uvar1(2+nt+1)
       shyd1 = uvar1(2+nt+2)
       wlim1 = uvar1(2+nt+3)
+c
+			wlimnorm  = uvar1(2+nt+4)
 c
 c                                     ---- get sdv and uvar after update
       se2 = uvar2(1)
@@ -287,9 +295,6 @@ c                                                       ---- update uvar
       uvar2(2+nt+3) = wlim2
       uvar2(2+nt+4) = wlim2/lim
 c
-c      											 ---- terminate analisys if limit is reached
-			if ( uvar1(2+nt+2) .ge. 1.0d0 ) call jancae_exit( 9000 )
-c
       return
       end
 c
@@ -300,37 +305,35 @@ c     Ayada
 c
       subroutine jancae_rup_ayada ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                              jmac,jmatyp,matlayo,laccfla,nt,
-     &                              ndrup,prrup )
+     &                              lim,wlimnorm )
 c
 c-----------------------------------------------------------------------
       implicit real*8 (a-h,o-z)
 c
       dimension UVAR1(NUVARM),JMAC(*),JMATYP(*)
-      dimension ARRAY(15),JARRAY(15),prrup(ndrup)
+      dimension ARRAY(15),JARRAY(15)
       dimension sdv(nsdv),uvar2(nuvarm)
       character*3 FLGRAY(15)
-      real*8 lim
+      real*8 lim,wlimnorm
       real*8 se1,peeq1,shyd1,shyd1se1,wlim1
       real*8 se2,peeq2,shyd2,shyd2se2,wlim2
 c-----------------------------------------------------------------------
 c
 c     nuvarm : 4
 c
+c     uvar(1)      : equivalent stress
 c     uvar(2+nt+1) : equivalent plastic strain
 c     uvar(2+nt+2) : hydrostatic stress
 c     uvar(2+nt+3) : rupture criterion
 c     uvar(2+nt+4) : rupture criterion normalised
 c
-c     prrup(1) : 4
-c     prrup(2) : rupture criterion limit
-c
-c                                           ---- rupture criterion limit
-      lim = prrup(2)
 c                                            ---- get uvar before update
       se1   = uvar1(1)
       peeq1 = uvar1(2+nt+1)
       shyd1 = uvar1(2+nt+2)
       wlim1 = uvar1(2+nt+3)
+c
+			wlimnorm  = uvar1(2+nt+4)
 c
 c                                     ---- get sdv and uvar after update
       se2 = uvar2(1)
@@ -360,9 +363,6 @@ c                                                       ---- update uvar
       uvar2(2+nt+3) = wlim2
       uvar2(2+nt+4) = wlim2/lim
 c
-c      											 ---- terminate analisys if limit is reached
-			if ( uvar1(2+nt+2) .ge. 1.0d0 ) call jancae_exit( 9000 )
-c
       return
       end
 c
@@ -373,39 +373,35 @@ c     Brozzo
 c
       subroutine jancae_rup_brozzo ( sdv,nsdv,uvar2,uvar1,nuvarm,jrcd,
      &                               jmac,jmatyp,matlayo,laccfla,nt,
-     &                               ndrup,prrup )
+     &                               lim,wlimnorm )
 c
 c-----------------------------------------------------------------------
       implicit real*8 (a-h,o-z)
 c
       dimension UVAR1(NUVARM),JMAC(*),JMATYP(*)
-      dimension ARRAY(15),JARRAY(15),prrup(ndrup)
+      dimension ARRAY(15),JARRAY(15)
       dimension sdv(nsdv),uvar2(nuvarm)
       character*3 FLGRAY(15)
-      real*8 lim
+      real*8 lim,wlimnorm
       real*8 se1,peeq1,shyd1,maxsp1,maxsp1shyd1,wlim1
       real*8 se2,peeq2,shyd2,maxsp2,maxsp2shyd2,wlim2
 c-----------------------------------------------------------------------
 c
 c     nuvarm : 5
 c
-c     uvar(2+nt+1) : eq. plastic strain
-c     uvar(2+nt+2) : max. principal stress
+c     uvar(2+nt+1) : equivalent plastic strain
+c     uvar(2+nt+2) : maximum principal stress
 c     uvar(2+nt+3) : hydrostatic stress
 c     uvar(2+nt+4) : rupture criterion
 c     uvar(2+nt+5) : rupture criterion normalised
-c
-c     prrup(1) : 5
-c     prrup(2) : rupture criterion limit
-c
-c                                           ---- rupture criterion limit
-      lim = prrup(2)
 c
 c                                            ---- get uvar before update
       peeq1  = uvar1(2+nt+1)
       maxsp1 = uvar1(2+nt+2)
       shyd1  = uvar1(2+nt+3)
       wlim1  = uvar1(2+nt+4)
+c
+			wlimnorm  = uvar1(2+nt+5)
 c
 c                                              ---- get sdv after update
       peeq2 = sdv(1)
@@ -422,7 +418,7 @@ c                                 ---- get principal stress after update
 c
 c                               ---- get hydrostatic stress after update
       call getvrm ('SINV',ARRAY,JARRAY,FLGRAY,JRCD,JMAC,JMATYP,
-     &             MATLAYO,LACCFLA )
+     &                    MATLAYO,LACCFLA )
       if ( JRCD .ne. 0 ) then
         write (6,*) 'request error in uvarm for sinv'
         write (6,*) 'stop in uvrm.'
@@ -448,9 +444,6 @@ c                                                       ---- update uvar
       uvar2(2+nt+3) = shyd2
       uvar2(2+nt+4) = wlim2
       uvar2(2+nt+5) = wlim2/lim
-c
-c      											 ---- terminate analisys if limit is reached
-			if ( uvar1(2+nt+2) .ge. 1.0d0 ) call jancae_exit( 9000 )
 c
       return
       end
