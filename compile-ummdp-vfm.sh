6280ef31dc58b741c0f6e0f7d36a0d65ff2bac94
Miguel G. Oliveira <37053606+migueljgoliveira@users.noreply.github.com>
2022-06-14 13:21:31 +0100

    Update compile-ummdp-vfm.sh

--- compile-ummdp-vfm.sh
+++ compile-ummdp-vfm.sh
@@ -8,6 +8,12 @@ mv source/tmp.f compiled/ummmdp_vfm.f
 
 cd compiled
 
-rm ummdp_vfm.pyd >nul
+rm ummdp_vfm.so >nul
 
 python -m numpy.f2py ummdp_vfm.f -m ummdp_vfm -h ummdp_vfm.pyf --overwrite-signature
+
+python SignatureFile.py
+
+python -m numpy.f2py -c ummdp_vfm2.pyf ummdp_vfm.f --fcompiler=intelem --opt="-heap-arrays -fast -QxHost" 1> ummdp_vfm.log
+
+mv ummdp_vfm.*.so ummdp_vfm.so >nul
